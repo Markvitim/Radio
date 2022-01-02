3bbcbbf6c2846c9b827bfd84ef865e60be1c434e
Markvitim <markvitim@gmail.com>
2022-01-02 15:02:42 +0300

    100% branches checked

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,6 +3,7 @@ package ru.netology.domain;
 
 public class Radio {
     private int curStation;
+    private int curVolume;
 
     public int getCurStation() {
         return curStation;
@@ -10,37 +11,57 @@ public class Radio {
 
     public void setCurStation(int curStation) {
         if (curStation > 9) {
-            return;
-        }
-        if (curStation < 0) {
-            return;
-        }
-        this.curStation = curStation;
-    }
-
-    public int setToMaxNumberStation() {
-        if (curStation <= 9) {
-            curStation--;
+            curStation = 0;
         }
         if (curStation < 0) {
             curStation = 9;
         }
         this.curStation = curStation;
+    }
+
+    public int setToMaxNumberStation() {
+        if (curStation > 0) {
+            curStation = curStation - 1;
+        }
         return curStation;
     }
 
 
     public int setToMinNumberStation() {
-        if (curStation <= 9) {
-            curStation++;
+        if (curStation < 9) {
+            curStation = curStation + 1;
         }
-        if (curStation > 9) {
-            curStation = 0;
-        }
-        this.curStation = curStation;
         return curStation;
     }
 
+    public int getCurVolume() {
+        return curVolume;
+    }
+
+    public void setCurVolume(int curVolume) {
+        if (curVolume > 10) {
+            curVolume = 10;
+        }
+        if (curVolume < 0) {
+            curVolume = 0;
+        }
+        this.curVolume = curVolume;
+    }
+
+    public int increaseVolume() {
+        if (curVolume < 10) {
+            curVolume++;
+        }
+        return curVolume;
+    }
+
+    public int reduceVolume() {
+        if (curVolume >0) {
+            curVolume--;
+        }
+        return curVolume;
+    }
 }
+
 
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -8,17 +8,17 @@ public class RadioTest {
     @Test
     public void shouldSetCurStation() {
         Radio radio = new Radio();
-        radio.setCurStation(1);
+        radio.setCurStation(-1);
         int actual = radio.getCurStation();
-        Assertions.assertEquals(1, actual);
+        Assertions.assertEquals(9, actual);
 
     }
 
     @Test
     public void shouldNextStation() {
         Radio radio = new Radio();
-        radio.setCurStation(9);
-        int expected = 0;
+        radio.setCurStation(0);
+        int expected = 1;
         int actual = radio.setToMinNumberStation();
         Assertions.assertEquals(expected, actual);
     }
@@ -26,9 +26,37 @@ public class RadioTest {
     @Test
     public void shouldPrevStation() {
         Radio radio = new Radio();
-        radio.setCurStation(0);
-        int expected = 9;
+        radio.setCurStation(9);
+        int expected = 8;
         int actual = radio.setToMaxNumberStation();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetVolume(){
+        Radio radio=new Radio();
+        radio.setCurVolume(11);
+        int expected=10;
+        int actual = radio.getCurVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolume(){
+        Radio radio=new Radio();
+        radio.setCurVolume(-1);
+        int expected=1;
+        int actual=radio.increaseVolume();
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldReduceVolume(){
+        Radio radio=new Radio();
+        radio.setCurVolume(0);
+        int expected=0;
+        int actual=radio.reduceVolume();
         Assertions.assertEquals(expected, actual);
     }
 }
